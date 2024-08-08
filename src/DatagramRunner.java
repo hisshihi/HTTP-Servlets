11c26649799d90b400877b455f6c1a8231b349ea
HissDev <hissoffc@gmail.com>
2024-08-08 18:08:58 +0300

    Добавил клиента и сервер UDP

---
+++ src/DatagramRunner.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class DatagramRunner {
+    public static void main(String[] args) throws IOException {
+        // UDP клиент
+        // Получаем объект InetAddress, представляющий хост с именем "localhost"
+        InetAddress inetAddresses = InetAddress.getByName("localhost");
+
+        // Создаем и автоматически закрываем DatagramSocket, используя try-with-resources
+        try (var datagramSocket = new DatagramSocket()) {
+            // Преобразуем строку сообщения в массив байтов
+            byte[] bytes = "Hello from UDP client".getBytes();
+
+            // Создаем DatagramPacket для отправки данных. Указываем массив байтов, его длину, адрес и порт
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, inetAddresses, 7777);
+
+            // Отправляем пакет через сокет
+            datagramSocket.send(packet);
+        }
+    }
+}
---
+++ src/DatagramServerRunner.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class DatagramServerRunner {
+    public static void main(String[] args) throws IOException {
+        // UDP сервер
+        // Создаем и автоматически закрываем DatagramSocket, привязанный к порту 7777, используя try-with-resources
+        try (DatagramSocket datagramSocket = new DatagramSocket(7777)) {
+            // Создаем буфер для приема данных размером 512 байтов
+            byte[] buffer = new byte[100];
+
+            // Создаем DatagramPacket для приема данных, используя созданный буфер
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            // Получаем данные в пакет через сокет
+            datagramSocket.receive(packet);
+
+            // Преобразуем полученные байты в строку и выводим на консоль
+            System.out.println(new String(buffer));
+        }
+    }
+}
+
