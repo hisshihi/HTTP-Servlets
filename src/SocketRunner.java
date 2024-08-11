da1bcb2e2f41954d57ba9c61c097d6631a24f53f
HissDev <hissoffc@gmail.com>
2024-08-12 02:07:26 +0300

    Добавил сервер, на который можно отправлять сообщения из SocketRunner.

--- src/SocketRunner.java
+++ src/SocketRunner.java
@@ -5,31 +5,49 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class SocketRunner {
 
     public static void main(String[] args) throws UnknownHostException {
 
         // Получаем IP-адрес для доменного имени "leon-studio.ru" с использованием IPv4
-        InetAddress inetAddress = Inet4Address.getByName("leon-studio.ru");
+        InetAddress inetAddress = Inet4Address.getByName("localhost");
 
         // Создаем сокет для соединения с указанным IP-адресом и портом 80 (типичный порт для HTTP)
         try (
                 // Создаем сокет для работы с TCP соединением
-                Socket socket = new Socket(inetAddress, 80);
+                Socket socket = new Socket(inetAddress, 7777);
                 // Создаем поток для отправки данных через сокет
                 DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
                 // Создаем поток для чтения данных через сокет
                 DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                Scanner scanner = new Scanner(System.in);
         ) {
+
+//            Добавляем мессанджер
+            while (scanner.hasNextLine()) {
+                // Отправляем запрос на сервер
+                String request = scanner.nextLine();
+                dataOutputStream.writeUTF(request);
+
+                // Получаем ответ
+                String response = dataInputStream.readUTF();
+                System.out.println("Request from server: " + response);
+            }
+
             // Отправляем строку "Hello World" через сокет
-            dataOutputStream.writeUTF("Hello World");
+//            dataOutputStream.writeUTF("Hello World");
 
             // Читаем все байты из входного потока
-            byte[] response = dataInputStream.readAllBytes();
+//            byte[] response = dataInputStream.readAllBytes();
 
-            // Выводим длину полученного ответа на экран
-            System.out.println(response.length);
+//            Получаем ответ от сервера
+//            String response = dataInputStream.readUTF();
+
+            // Выводим ответ
+//            System.out.println("Response from server: " + response);
         } catch (IOException e) {
             // Если возникает ошибка ввода-вывода, выбрасываем исключение
             throw new RuntimeException(e);
---
+++ src/SocketServerRunner.java
@@ -0,0 +1,42 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServerRunner {
+    public static void main(String[] args) {
+//        Является хостом, к котому будут идти запросы клиентов.
+        try (
+                ServerSocket serverSocket = new ServerSocket(7777);
+//                Кто подключился
+                Socket socket = serverSocket.accept();
+//                Поток ввода
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+//                Поток вывода
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+
+                Scanner scanner = new Scanner(System.in);
+        ) {
+
+            String request = inputStream.readUTF();
+            String helloMessage = "Hello, I'm a server. Write \"stop\" so that I stop.";
+            outputStream.writeUTF(helloMessage);
+            while (!"stop".equals(request)) {
+//            Читаем строку от клиента
+//                String clientMessage = inputStream.readUTF();
+                System.out.println("Client request: " + request);
+
+                String response = scanner.nextLine();
+
+                // Отправляем ему ответ
+                outputStream.writeUTF(response);
+                request = inputStream.readUTF();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
