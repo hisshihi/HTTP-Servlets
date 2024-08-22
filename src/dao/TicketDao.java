08478504c16e4efa121cbbdd20476700a9bec47d
HissDev <hissoffc@gmail.com>
2024-08-22 19:39:01 +0300

    Добавил и доработал оставшиеся объекты для вывода билетов. В данном случае выводится только номер места. Также убрал вывод в TicketServlet в консоль.

--- src/dao/TicketDao.java
+++ src/dao/TicketDao.java
@@ -1,18 +1,63 @@
 package dao;
 
 import entity.TicketEntity;
+import util.ConnectionManager;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class TicketDao implements Dao<Long, TicketEntity> {
 
     private static final TicketDao INSTANCE = new TicketDao();
+    private static final String FIND_ALL_BY_FLIGHT_ID = """
+        SELECT *
+        FROM ticket
+        WHERE flight_id = ?
+    """;
 
-    private TicketDao() {}
+    private TicketDao() {
+    }
 
     public static TicketDao getInstance() {
         return INSTANCE;
+    }
+
+    // Поиск билетов по id перелёта
+    public List<TicketEntity> findAllByFlightId(Long flightId) {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_BY_FLIGHT_ID)) {
+            preparedStatement.setObject(1, flightId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<TicketEntity> tickets = new ArrayList<>();
+
+            while (resultSet.next()) {
+                tickets.add(buildticket(resultSet));
+            }
+
+            return tickets;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private TicketEntity buildticket(ResultSet resultSet) throws SQLException {
+        return new TicketEntity(
+          resultSet.getObject("id", Long.class),
+          resultSet.getObject("passenger_no", String.class),
+          resultSet.getObject("passenger_name", String.class),
+          resultSet.getObject("flight_id", Long.class),
+          resultSet.getObject("seat_no", String.class),
+          resultSet.getObject("cost", BigDecimal.class)
+        );
     }
 
     @Override
--- src/servlet/TicketServlet.java
+++ src/servlet/TicketServlet.java
@@ -5,34 +5,33 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import service.FlightService;
+import service.TicketService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet("/flights")
-public class FlightServlet extends HttpServlet {
+@WebServlet("/tickets")
+public class TicketServlet extends HttpServlet {
 
-    private final FlightService flightService = FlightService.getInstance();
+    private final TicketService ticketService = TicketService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         resp.setContentType("text/html");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
+        Long flightId = Long.valueOf(req.getParameter("flightId"));
+
         try (PrintWriter printWriter = resp.getWriter()) {
-            printWriter.write("<h1>Список перелётов:</h1>");
+            printWriter.write("<h1>Купленные билеты:</h1>");
             printWriter.write("<ul>");
-            flightService.findAll().forEach(flightDto -> {
-                printWriter.write(
-                        """
-                                <li>
-                                    <a href="/tickets?flightId=%d">%s</a>
-                                </li>
-                                """.formatted(flightDto.getId(), flightDto.getDescription())
-                );
-            });
+            ticketService.findAllByFlightId(flightId).forEach(ticketDto -> printWriter.write("""
+                        <li>
+                            %s
+                        </li>
+                    """.formatted(ticketDto.getSeatNo())));
             printWriter.write("</ul>");
         }
     }
